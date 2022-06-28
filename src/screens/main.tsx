3db6dbe28fd8e4bd2c802256b67202e671bba6f8
Lok Yiu Yeung <ryanyeung@Loks-MacBook-Air.local>
2022-06-28 18:36:42 +0800

    second commit

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, {useCallback, useState } from 'react';
+import { Pressable } from 'react-native';
 import {
   Text,
   Box,
@@ -6,13 +7,25 @@ import {
   VStack,
   useColorModeValue
 } from 'native-base'
+import AnimatedCheckbox from '../components/animated-checkbox';
+import TaskItem from '../components/task-item';
 import ThemeToggle from '../components/theme-toggle';
 
 export default function MainScreen () {
+  const [checked, setChecked] = useState(false);
+  const handlePressCheckbox = useCallback(() => {
+    setChecked(prev => !prev)
+  }, [])
+  
   return (
     <Center _dark={{ bg:'blueGray.900'}} _light={{ bg: 'blueGray.50'}} px={4} flex={1}>
       <VStack space={5} alignItems="center">
-        <Box bg={useColorModeValue('red.500', 'yellow.500')}>
+        <Box w="100px" h="100px">
+        <Pressable onPress={handlePressCheckbox}>
+          <AnimatedCheckbox />
+        </Pressable>
+        </Box>
+        <Box bg={useColorModeValue('red.500', 'yellow.500')} p={10}>
           <Text>Hello!</Text>
         </Box>
         <ThemeToggle />
