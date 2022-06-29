7ab376ec941dfe13baf3571319e85de7f7dd479f
Lok Yiu Yeung <ryanyeung@Loks-MacBook-Air.local>
2022-06-29 14:17:52 +0800

    change App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import AppContainer from './src/components/app-container';
-import Main from './src/screens/main-screen';
+import Navigator from './src/';
 
 export default function App() {
   return (
     <AppContainer>
-      <Main />
+      <Navigator />
     </AppContainer>
   );
 }
